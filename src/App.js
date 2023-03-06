a6916a7452bfeb27dca204094c284f5431223fc7
svetlanael12 <svetlana_el12@mail.ru>
2023-03-06 20:45:59 +0500

    class-based

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
+import React, { Component } from 'react';
+import ShopItemClass from './components/shopItemClass';
 import './App.css';
-import ShopItemFunc from './components/shopItemFunc';
 
-function App() {
-  const item = {
-    brand: 'Tiger of Sweden',
-    title: 'Leonard coat',
-    description: 'Minimalistic coat in cotton-blend',
-    descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
-    price: 399,
-    currency: '£'
+export default class App extends Component {
+  render() {
+    const item = {
+      brand: 'Tiger of Sweden',
+      title: 'Leonard coat',
+      description: 'Minimalistic coat in cotton-blend',
+      descriptionFull: 'Men\'s minimalistic overcoat in cotton-blend. Features a stand-up collar, concealed front closure and single back vent. Slim fit with clean, straight shape. Above-knee length.',
+      price: 399,
+      currency: '£'
+    }
+    return (
+      <div className="container">
+        <div className="background-element">
+        </div>
+        <div className="highlight-window">
+          <div className='highlight-overlay'></div>
+        </div>
+        <div className="window">
+          <ShopItemClass item={item} />
+        </div>
+      </div>
+    )
   }
-  return (
-    <div className="container">
-      <div className="background-element">
-      </div>
-      <div className="highlight-window">
-        <div className='highlight-overlay'></div>
-      </div>
-      <div className="window">
-        <ShopItemFunc item={item} />
-      </div>
-    </div>
-  );
 }
-
-export default App;
